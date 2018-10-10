8d0dd86b1d112ef20635e71939d093fd2ac7ed13
lucazbrand <lucazbrandao@gmail.com>
2018-10-10 20:41:04 -0300

    Adicionado interface inicial e nova Activity para registrar lembrete

---
+++ app/src/main/java/ufjf/dcc196_2018_3/ex02lucasbrandao/RegistrarActivity.java
@@ -0,0 +1,13 @@
+package ufjf.dcc196_2018_3.ex02lucasbrandao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+    }
+}
